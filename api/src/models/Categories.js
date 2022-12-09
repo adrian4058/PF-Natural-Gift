64a663005a182bcc706256f3e7bd8afff5e242f1
Exequiel Rodriguez <109878818+Kitatzu@users.noreply.github.com>
2022-12-09 00:45:34 -0300

    Update Categories.js

--- api/src/models/Categories.js
+++ api/src/models/Categories.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const categoriesSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type:String,
+    unique: true
 });
 
 const Categories = mongoose.model("categories", categoriesSchema);
